7f69ab7cecb273a4a775fec215c8568c7b0bf6fa
anxiety16 <145312928+anxiety16@users.noreply.github.com>
2023-09-21 11:18:43 +0800

    i love you sir (#1)

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
-print('this is model')
+class student:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+
+    def details(self):
+        print("{} {} - {}".format(self.first, self.last, self.age))
+
+std = student("Lexus", "Taladro", 22)
+std.details()
